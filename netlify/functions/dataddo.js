456cc99758f49b42bded19dbd2032bb82de0405a
lucia2311-qm <lucia.mazzeo@qm-e.com>
2026-06-19 09:40:20 -0300

    Update Dataddo function to fetch new data source

--- netlify/functions/dataddo.js
+++ netlify/functions/dataddo.js
@@ -1,25 +1,28 @@
 
+
 exports.handler = async function () {
   try {
     const response = await fetch(
-      "https://data.eu-west-1.aws.dataddo.com/v1.0/get/6846ff442831cfa6110ce71c?type=csv",
+      "https://data.eu-west-1.aws.dataddo.com/v1.0/get/6942b1ee3dc1bc38690e091d?type=csv",
       {
+        method: "GET",
         headers: {
-          Authorization: `Bearer ${process.env.DATADDO_TOKEN}`,
-        },
+          "Authorization": "Bearer " + process.env.DATADDO_TOKEN
+        }
       }
     );
 
-    const data = await response.text();
+    const text = await response.text();
 
     return {
       statusCode: 200,
-      body: data,
+      body: text
     };
+
   } catch (error) {
     return {
       statusCode: 500,
-      body: "Error al obtener datos",
+      body: JSON.stringify(error.message)
     };
   }
 };
